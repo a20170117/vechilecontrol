687c82cf9edd80e466c4416a1c58b1efbd8e74ac
zhaoyouran <yr.zhao@bmi-tech.cn>
2019-01-29 12:59:49 +0800

    为履带车添加注释

--- VechileCaterpillarTrack.py
+++ VechileCaterpillarTrack.py
@@ -4,11 +4,19 @@ from Vechile import Vechile
 class Caterpillar(Vechile):
     
     def move_forward(self, e: MoveForward, value: float):
+        """
+        value 正为前进，负为后退
+        """
         pass
 
     def turn(self, e: Turn, value: float):
+        """
+        value 负为左转，正为右转
+        """
         pass
 
+    def stop(self, e: Stop, value: float):
+        pass
 
     def __forward(self):
         pass
